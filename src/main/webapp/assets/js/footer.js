db0437e4450eed8b71f172519fc57d5db7d309e1
KKRYOUN <kkryoun300@gmail.com>
2023-10-17 18:29:28 +0900

    김경륜 : 톰켓 서버 실행 파일과 footer 수정, css 파일을 업데이트 했습니다.

--- src/main/webapp/assets/js/footer.js
+++ src/main/webapp/assets/js/footer.js
@@ -1,6 +1,6 @@
 // XMLHttpRequest 객체를 사용하여 footer.HTML 파일을 가져옴
 var footer = new XMLHttpRequest();
-footer.open('GET', '../../views/news/footer.html', true);
+footer.open('GET', '../footer.html', true);
 footer.onreadystatechange = function() {
   if (footer.readyState === 4 && footer.status === 200) {
     // footer.HTML 파일의 내용을 가져와서 <div>에 삽입

53dff665133fec7ef0d3b754bb6aa6f35f2b1636
yesri <cyesri281@gmail.com>
2023-10-18 18:46:00 +0900

    최예슬이 : 부트스트랩을 이용하여 배치 수정

--- src/main/webapp/assets/js/job/job_view_map.js
+++ src/main/webapp/assets/js/job/job_view_map.js
@@ -1,9 +1,18 @@
 function initializeMap() {
-    var mapContainer = document.getElementById('map');
+    var mapContainer = document.getElementById("map");
     var mapOption = {
-        center: new kakao.maps.LatLng(37.56601, 126.97888),
+        center: new kakao.maps.LatLng(37.498786, 127.031673),
         level: 4,
         mapTypeId: kakao.maps.MapTypeId.ROADMAP
     };
     var map = new kakao.maps.Map(mapContainer, mapOption);
+    
+    var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667);  // 마커가 표시될 위치 
+	var marker = new kakao.maps.Marker({  // 마커를 생성한다
+	    position: markerPosition
+	});
+
+	marker.setMap(map); // 마커가 지도 위에 표시되도록 설정한다
+
+	});
 }

34f94f014087fee89b5e6676052a32d74a3f818b
yesri <cyesri281@gmail.com>
2023-10-19 18:26:27 +0900

    최예슬이 : job_view 수정

--- src/main/webapp/assets/js/job/job_view_map.js
+++ src/main/webapp/assets/js/job/job_view_map.js
@@ -1,18 +1,23 @@
 function initializeMap() {
-    var mapContainer = document.getElementById("map");
-    var mapOption = {
-        center: new kakao.maps.LatLng(37.498786, 127.031673),
-        level: 4,
-        mapTypeId: kakao.maps.MapTypeId.ROADMAP
-    };
-    var map = new kakao.maps.Map(mapContainer, mapOption);
-    
-    var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667);  // 마커가 표시될 위치 
-	var marker = new kakao.maps.Marker({  // 마커를 생성한다
-	    position: markerPosition
-	});
+   var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+		    mapOption = {
+		        center: new kakao.maps.LatLng(37.498832, 127.031755), // 지도의 중심좌표
+		        level: 3, // 지도의 확대 레벨
+		        mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
+		    }; 
 
-	marker.setMap(map); // 마커가 지도 위에 표시되도록 설정한다
+		// 지도를 생성한다 
+		var map = new kakao.maps.Map(mapContainer, mapOption); 
 
-	});
+		// 지도에 확대 축소 컨트롤을 생성한다
+		var zoomControl = new kakao.maps.ZoomControl();
+
+		// 지도의 우측에 확대 축소 컨트롤을 추가한다
+		map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+		// 지도에 마커를 생성하고 표시한다
+		var marker = new kakao.maps.Marker({
+		    position: new kakao.maps.LatLng(37.498832, 127.031755), // 마커의 좌표
+		    map: map // 마커를 표시할 지도 객체
+		});
 }
